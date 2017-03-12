5e7418d64cb65c333aae5b633b776db3cb0539bc
Bianca <bianca.rc@hotmail.com>
2017-03-11 23:36:15 -0300

    Comentários com tags Doxygen

--- include/area.h
+++ include/area.h
@@ -5,42 +5,62 @@
 
 float AreaTriangulo (float base, float altura);
 /**
- * @brief Calcula e retorna a área do triângulo
+ * @brief Calcula a área do triângulo
+ * @param base Base do triângulo
+ * @param altura Altura do triângulo
+ * @return Área do triângulo
  */
 
 float AreaRetangulo (float base, float altura);
 /**
- * @brief Calcula e retorna a área do retângulo
+ * @brief Calcula a área do retângulo
+ * @param base Base do retângulo
+ * @param altura Altura do retângulo
+ * @return Área do retângulo
  */
 
 float AreaQuadrado (float lado);
 /**
- * @brief Calcula e retorna a área do quadrado
+ * @brief Calcula a área do quadrado
+ * @param lado Lado do quadrado
+ * @return Área do quadrado
  */
 
 float AreaCirculo (float raio);
 /**
- * @brief Calcula e retorna a área do círculo
+ * @brief Calcula a área do círculo
+ * @param raio Raio do círculo
+ * @return Área do círculo
  */
-
 float AreaPiramide (float base, float altura);
 /**
- * @brief Calcula e retorna a área da pirâmide
+ * @brief Calcula a área da pirâmide
+ * @param base Base da pirâmide
+ * @param altura Altura da pirâmide
+ * @return Área da pirâmide
  */
 
 float AreaCubo (float lado);
 /**
- * @brief Calcula e retorna a área do cubo
+ * @brief Calcula a área do cubo
+ * @param lado Lado do cubo
+ * @return Área do cubo
  */
 
 float AreaParalelepipedo (float base, float altura, float largura);
 /**
- * @brief Calcula e retorna a área do paralelepípedo
+ * @brief Calcula a área do paralelepípedo
+ * @param base Base do paralelepípedo
+ * @param altura Altura do paralelepípedo
+ * @param largura Largura do paralelepípedo
+ * @return Área do paralelepípedo
  */
 
 float AreaEsfera (float raio);
 /**
- * @brief Calcula e retorna a área do esfera
+ * @brief Calcula a área da esfera
+ * @param raio Raio da esfera
+ * @return Área da esfera
  */
 
 #endif
